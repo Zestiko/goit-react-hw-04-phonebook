918e0ab0bf28f7e30c0623cd3b07c009d985aa5d
Alieksieiev Vladyslav <kat9.hora@gmail.com>
2023-02-01 20:07:36 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,23 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, } from 'react';
 import { nanoid } from 'nanoid';
 import { Form } from './Form/Form';
 import { Contacts } from './Contacts/Contacts';
 import { Filter } from './Filter/Filter';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(() =>
-    JSON.parse(localStorage.getItem('contactsData'))
-      ? JSON.parse(localStorage.getItem('contactsData'))
-      : [
-          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-        ]
+  const [contacts, setContacts] = useState(
+    () =>
+      JSON.parse(localStorage.getItem('contactsData')) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
   );
-  // const [contacts, setContacts] = useState(() => [
-  //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  // ]);
   const [filter, setFilter] = useState('');
-  const itsFirstRender = useRef(true);
+
 
   useEffect(() => {
-    if (itsFirstRender.current) {
-      itsFirstRender.current = false;
-      return;
-    }
     localStorage.setItem('contactsData', JSON.stringify(contacts));
   }, [contacts]);
 
@@ -73,7 +62,7 @@ export const App = () => {
 
       <h2>Contacts</h2>
       <Filter value={filter} onChange={handelChange} />
-      <Contacts contacts={filterContacts} onDelete={handleDelete} />
+      {<Contacts contacts={filterContacts} onDelete={handleDelete} />}
     </>
   );
 };
